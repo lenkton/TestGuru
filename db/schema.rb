6f591f75c0942bc1e7246f20534ca5ac7bffc0c9
lenkton <antonov_s@yahoo.com>
2020-11-28 18:34:15 +0300

    migrated (according to the preceeding migration)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_152920) do
+ActiveRecord::Schema.define(version: 2020_11_28_153106) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2020_11_28_152920) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
