243383972f299da7e873dc462a0e657f95920bf1
lenkton <antonov_s@yahoo.com>
2020-11-28 18:36:54 +0300

    added null-restriction on question_id, text and user_id of an answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_153106) do
+ActiveRecord::Schema.define(version: 2020_11_28_153232) do
 
   create_table "answers", force: :cascade do |t|
-    t.string "text"
-    t.integer "question_id"
-    t.integer "user_id"
+    t.string "text", null: false
+    t.integer "question_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
