9f8bcd47dbfa3d2b05d770e1acf8c8e3dd0b26dc
lenkton <antonov_s@yahoo.com>
2020-11-28 23:53:40 +0300

    added a level field to the test model (with the default value of 0)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_153232) do
+ActiveRecord::Schema.define(version: 2020_11_28_205128) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_11_28_153232) do
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "level", default: 0
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
