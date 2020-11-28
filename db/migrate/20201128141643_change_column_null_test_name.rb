34c21b23dd41a3546811cf668b35dd73acb3fdb0
lenkton <antonov_s@yahoo.com>
2020-11-28 18:27:57 +0300

    fixes and additional migrations (so now none of the test and category fields can be null)

--- db/migrate/20201128141643_change_column_null_test_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnNullTestName < ActiveRecord::Migration[6.0]
-  def change
-    change_column_null :tests, :name, false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_28_141643) do
+ActiveRecord::Schema.define(version: 2020_11_28_151114) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 2020_11_28_141643) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_11_28_141643) do
 
   create_table "tests", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "category_id"
+    t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
