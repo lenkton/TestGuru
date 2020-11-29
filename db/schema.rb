f4940e04e9f928811db34db910f8991e2bcbf5b6
lenkton <antonov_s@yahoo.com>
2020-11-29 20:08:26 +0300

    added an author_id column to the table 'test'

--- db/schema.rb
+++ db/schema.rb
@@ -38,10 +38,12 @@ ActiveRecord::Schema.define(version: 2020_11_25_190651) do
   create_table "tests", force: :cascade do |t|
     t.string "name", null: false
     t.integer "category_id", null: false
+    t.integer "user_id", null: false
     t.integer "level", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,4 +55,5 @@ ActiveRecord::Schema.define(version: 2020_11_25_190651) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users"
 end
