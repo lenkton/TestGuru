f7a443374019900b7ee10ff6209f271e20de1658
lenkton <antonov_s@yahoo.com>
2020-11-29 18:38:57 +0300

    Removed a link to user from the answer model

--- db/migrate/20201125190651_create_answers.rb
+++ db/migrate/20201125190651_create_answers.rb
@@ -3,7 +3,6 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
     create_table :answers do |t|
       t.string :text, null: false
       t.references :question, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
       t.boolean :correct, default: false, null: false
 
       t.timestamps
