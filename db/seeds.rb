06756aa9a80096af28e8d4449619543cdf26aec5
lenkton <antonov_s@yahoo.com>
2020-12-01 20:53:22 +0300

    added some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.create([
+                  { name: 'Frontend' },
+                  { name: 'Backend' },
+                  { name: 'Business' }
+                ])
+User.create([
+              { name: 'Thinknetica' },
+              { name: 'Bill Gates' },
+              { name: 'Sergey Antonov' }
+            ])
+Test.create([
+              { name: 'Ruby for beginners', category_id: 2, user_id: 1, level: 0 },
+              { name: 'Getting Rich: bird-eye perspective', category_id: 3, user_id: 2, level: 1 },
+              { name: "How to write computer programs in HTML: Tips for Pro's", category_id: 1, user_id: 3, level: 3 }
+            ])
+Question.create([
+                  { text: 'Test question #11', test_id: 1 },
+                  { text: 'Test question #21', test_id: 1 },
+                  { text: 'Test question #31', test_id: 1 },
+
+                  { text: 'Test question #12', test_id: 2 },
+                  { text: 'Test question #22', test_id: 2 },
+                  { text: 'Test question #32', test_id: 2 },
+
+                  { text: 'Test question #13', test_id: 3 },
+                  { text: 'Test question #23', test_id: 3 },
+                  { text: 'Test question #33', test_id: 3 }
+                ])
+Answer.create([
+                { text: 'The right answer', question_id: 1, correct: true },
+                { text: 'A wrong answer', question_id: 1, correct: false },
+                { text: 'The right answer', question_id: 2, correct: true },
+                { text: 'A wrong answer', question_id: 2, correct: false },
+                { text: 'The right answer', question_id: 3, correct: true },
+                { text: 'A wrong answer', question_id: 3, correct: false },
+
+                { text: 'The right answer', question_id: 4, correct: true },
+                { text: 'A wrong answer', question_id: 4, correct: false },
+                { text: 'The right answer', question_id: 5, correct: true },
+                { text: 'A wrong answer', question_id: 5, correct: false },
+                { text: 'The right answer', question_id: 6, correct: true },
+                { text: 'A wrong answer', question_id: 6, correct: false },
+
+                { text: 'The right answer', question_id: 7, correct: true },
+                { text: 'A wrong answer', question_id: 7, correct: false },
+                { text: 'The right answer', question_id: 8, correct: true },
+                { text: 'A wrong answer', question_id: 8, correct: false },
+                { text: 'The right answer', question_id: 9, correct: true },
+                { text: 'A wrong answer', question_id: 9, correct: false }
+              ])
