e76dae17e4a5caa57eaf8ad4b68a23cb34945e26
lenkton <antonov_s@yahoo.com>
2020-12-02 01:16:03 +0300

    some fixes

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
   def self.from_category(category)
-    Test.includes(:category).where(categories: { name: category }).distinct.pluck(:name).sort
+    Test.includes(:category).where(categories: { name: category }).distinct.pluck(:name).sort.reverse!
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   def participated_tests(level)
-    TestTakingSession.where(user_id: id).includes(:test).where(tests: { level: level }).map(&:test)
+    TestTakingSession.where(user_id: id).includes(:test).where(tests: { level: level }).distinct.map(&:test)
   end
 end
