5a47d2e35126fa8f922cccf196bfe865d22939de
lenkton <antonov_s@yahoo.com>
2020-12-04 20:43:50 +0300

    style fixes in test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   def self.from_category(category)
     Test.joins('JOIN categories ON tests.category_id = categories.id')
-        .where('categories.name = ?', category).order(name: :desc).pluck(:name)
+        .where(categories: { name: category }).order(name: :desc).pluck(:name)
   end
 end
