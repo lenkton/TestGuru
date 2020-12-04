88625bfbdce81e49b6fe66dcf7463b404e670db3
lenkton <antonov_s@yahoo.com>
2020-12-04 19:24:46 +0300

    in seeds.rb changed hard-coded id's into id's of the elements of arrays

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,55 +1,68 @@
-Category.create!([
-                   { name: 'Frontend' },
-                   { name: 'Backend' },
-                   { name: 'Business' }
-                 ])
-User.create!([
-               { name: 'Thinknetica' },
-               { name: 'Bill Gates' },
-               { name: 'Sergey Antonov' }
-             ])
-Test.create!([
-               { name: 'Ruby for beginners', category_id: 2, user_id: 1, level: 0 },
-               { name: 'Getting Rich: bird-eye perspective', category_id: 3, user_id: 2, level: 1 },
-               { name: "How to write computer programs in HTML: Tips for Pro's", category_id: 1, user_id: 3, level: 3 }
-             ])
-Question.create!([
-                   { text: 'Test question #11', test_id: 1 },
-                   { text: 'Test question #21', test_id: 1 },
-                   { text: 'Test question #31', test_id: 1 },
+categories =
+  Category.create!([
+                     { name: 'Frontend' },
+                     { name: 'Backend' },
+                     { name: 'Business' }
+                   ])
+users =
+  User.create!([
+                 { name: 'Thinknetica' },
+                 { name: 'Bill Gates' },
+                 { name: 'Sergey Antonov' }
+               ])
+tests =
+  Test.create!([
+                 { name: 'Ruby for beginners',
+                   category_id: categories[2].id,
+                   user_id: users[1].id,
+                   level: 0 },
+                 { name: 'Getting Rich: bird-eye perspective',
+                   category_id: categories[3].id,
+                   user_id: users[2].id,
+                   level: 1 },
+                 { name: "How to write computer programs in HTML: Tips for Pro's",
+                   category_id: categories[1].id,
+                   user_id: users[3].id,
+                   level: 3 }
+               ])
+questions =
+  Question.create!([
+                     { text: 'Test question #11', test_id: tests[1].id },
+                     { text: 'Test question #21', test_id: tests[1].id },
+                     { text: 'Test question #31', test_id: tests[1].id },
 
-                   { text: 'Test question #12', test_id: 2 },
-                   { text: 'Test question #22', test_id: 2 },
-                   { text: 'Test question #32', test_id: 2 },
+                     { text: 'Test question #12', test_id: tests[2].id },
+                     { text: 'Test question #22', test_id: tests[2].id },
+                     { text: 'Test question #32', test_id: tests[2].id },
 
-                   { text: 'Test question #13', test_id: 3 },
-                   { text: 'Test question #23', test_id: 3 },
-                   { text: 'Test question #33', test_id: 3 }
-                 ])
+                     { text: 'Test question #13', test_id: tests[3].id },
+                     { text: 'Test question #23', test_id: tests[3].id },
+                     { text: 'Test question #33', test_id: tests[3].id }
+                   ])
 Answer.create!([
-                 { text: 'The right answer', question_id: 1, correct: true },
-                 { text: 'A wrong answer', question_id: 1, correct: false },
-                 { text: 'The right answer', question_id: 2, correct: true },
-                 { text: 'A wrong answer', question_id: 2, correct: false },
-                 { text: 'The right answer', question_id: 3, correct: true },
-                 { text: 'A wrong answer', question_id: 3, correct: false },
+                 { text: 'The right answer', question_id: questions[1].id, correct: true },
+                 { text: 'A wrong answer', question_id: questions[1].id, correct: false },
+                 { text: 'The right answer', question_id: questions[2].id, correct: true },
+                 { text: 'A wrong answer', question_id: questions[2].id, correct: false },
+                 { text: 'The right answer', question_id: questions[3].id, correct: true },
+                 { text: 'A wrong answer', question_id: questions[3].id, correct: false },
 
-                 { text: 'The right answer', question_id: 4, correct: true },
-                 { text: 'A wrong answer', question_id: 4, correct: false },
-                 { text: 'The right answer', question_id: 5, correct: true },
-                 { text: 'A wrong answer', question_id: 5, correct: false },
-                 { text: 'The right answer', question_id: 6, correct: true },
-                 { text: 'A wrong answer', question_id: 6, correct: false },
+                 { text: 'The right answer', question_id: questions[4].id, correct: true },
+                 { text: 'A wrong answer', question_id: questions[4].id, correct: false },
+                 { text: 'The right answer', question_id: questions[5].id, correct: true },
+                 { text: 'A wrong answer', question_id: questions[5].id, correct: false },
+                 { text: 'The right answer', question_id: questions[6].id, correct: true },
+                 { text: 'A wrong answer', question_id: questions[6].id, correct: false },
 
-                 { text: 'The right answer', question_id: 7, correct: true },
-                 { text: 'A wrong answer', question_id: 7, correct: false },
-                 { text: 'The right answer', question_id: 8, correct: true },
-                 { text: 'A wrong answer', question_id: 8, correct: false },
-                 { text: 'The right answer', question_id: 9, correct: true },
-                 { text: 'A wrong answer', question_id: 9, correct: false }
+                 { text: 'The right answer', question_id: questions[7].id, correct: true },
+                 { text: 'A wrong answer', question_id: questions[7].id, correct: false },
+                 { text: 'The right answer', question_id: questions[8].id, correct: true },
+                 { text: 'A wrong answer', question_id: questions[8].id, correct: false },
+                 { text: 'The right answer', question_id: questions[9].id, correct: true },
+                 { text: 'A wrong answer', question_id: questions[9].id, correct: false }
                ])
 TestTakingSession.create!([
-                            { user_id: 3, test_id: 1 },
-                            { user_id: 3, test_id: 2 },
-                            { user_id: 3, test_id: 3 }
+                            { user_id: users[3].id, test_id: tests[1].id },
+                            { user_id: users[3].id, test_id: tests[2].id },
+                            { user_id: users[3].id, test_id: tests[3].id }
                           ])
