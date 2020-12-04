40f4b70e5b0e1e5011627837d4a8283bffc01197
lenkton <antonov_s@yahoo.com>
2020-12-05 01:40:46 +0300

    fixed the User model's participated_tests method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def participated_tests(level)
     Test.joins('JOIN test_taking_sessions ON tests.id = test_taking_sessions.test_id')
-        .where(level: level).where(user_id: id)
+        .where(level: level).where(test_taking_sessions: { user_id: id })
   end
 end
