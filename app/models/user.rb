feafe18e0365d531239a46e560886f76032df3ce
lenkton <antonov_s@yahoo.com>
2020-12-06 19:31:06 +0300

    updated participated_tests (in the User model) with accordance to the new interface

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   has_many :taken_tests, through: :test_taking_sessions, source: :test
 
   def participated_tests(level)
-    Test.joins('JOIN test_taking_sessions ON tests.id = test_taking_sessions.test_id')
-        .where(level: level).where(test_taking_sessions: { user_id: id })
+    taken_tests.where(level: level)
   end
 end
