2570eea95f8659fcddbad6d0602617927afd8112
lenkton <antonov_s@yahoo.com>
2020-12-06 15:01:36 +0300

    added User(author)[1..1] - [0..n]Test association

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author, class_name: :User
 
   def self.from_category(category)
     Test.joins('JOIN categories ON tests.category_id = categories.id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :tests, foreign_key: :author_id
+
   def participated_tests(level)
     Test.joins('JOIN test_taking_sessions ON tests.id = test_taking_sessions.test_id')
         .where(level: level).where(test_taking_sessions: { user_id: id })
