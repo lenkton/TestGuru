cd4e79cb2ebe92c8dde76fb13f9b0496d7a73403
lenkton <antonov_s@yahoo.com>
2020-12-06 19:11:46 +0300

    fix: added :dependent parameter to all the has_many method calls

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: :User
-  has_many :questions
-  has_many :test_taking_sessions
+  has_many :questions, dependent: :destroy
+  has_many :test_taking_sessions, dependent: :destroy
   has_many :test_takers, through: :test_taking_sessions, source: :user
 
   def self.from_category(category)
