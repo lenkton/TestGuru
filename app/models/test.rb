8699121cd50962e3fcc666b26fbdffeaf0116b3b
lenkton <antonov_s@yahoo.com>
2020-12-06 15:18:36 +0300

    added Test [1..1] - [0..n] Questions association

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: :User
+  has_many :questions
 
   def self.from_category(category)
     Test.joins('JOIN categories ON tests.category_id = categories.id')
