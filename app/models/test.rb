80daf8d53843235feefdc7dd34871c60702530b5
lenkton <antonov_s@yahoo.com>
2020-12-06 14:21:17 +0300

    added Category[1..1] - [0..n]Test relation

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,6 @@
 class Test < ApplicationRecord
+  belongs_to :category
+
   def self.from_category(category)
     Test.joins('JOIN categories ON tests.category_id = categories.id')
         .where(categories: { name: category }).order(name: :desc).pluck(:name)
