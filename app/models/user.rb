20abc1c19b92cbdf35f06c31b7f2f98c00aad2e1
lenkton <antonov_s@yahoo.com>
2020-12-06 23:14:34 +0300

    in has_many relations set parameter :dependent to :destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :tests, foreign_key: :author_id, dependent: :restrict_with_exception
+  has_many :tests, foreign_key: :author_id, dependent: :destroy
   has_many :test_taking_sessions, dependent: :destroy
   has_many :taken_tests, through: :test_taking_sessions, source: :test
 
