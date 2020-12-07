abfc04ee24f378c80a73d7005cf804ceb5bd6345
lenkton <antonov_s@yahoo.com>
2020-12-07 19:36:59 +0300

    added scope methods 'easy' (selects tests with the level 0 or 1), 'medium' (levels 2 - 4), 'hard' (levels 5+) for the Test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,10 @@ class Test < ApplicationRecord
   has_many :test_taking_sessions, dependent: :destroy
   has_many :test_takers, through: :test_taking_sessions, source: :user
 
+  scope :easy, -> { where(level: [0..1]) }
+  scope :medium, -> { where(level: [2..4]) }
+  scope :hard, -> { where(level: [5..Float::INFINITY]) }
+
   def self.from_category(category)
     Category.find_by(name: category).tests.order(name: :desc).pluck(:name)
   end
