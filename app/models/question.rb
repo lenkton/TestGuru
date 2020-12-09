42dd5ed0f8e8a85341dfb0cd3ad3f36d227aec2b
lenkton <antonov_s@yahoo.com>
2020-12-09 13:29:33 +0300

    deleted the unnessessary inverse_of in the Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :test
-  
-  has_many :answers, dependent: :destroy, inverse_of: :question
+
+  has_many :answers, dependent: :destroy
 
   validates :text, :test, presence: true
   validates :answers, length: { in: 1..4 }
