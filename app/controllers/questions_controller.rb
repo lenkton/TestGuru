a0f073ca2217a7abfd4d60e6914351b6f9937cb4
lenkton <antonov_s@yahoo.com>
2020-12-11 21:36:49 +0300

    added question inspection (in the Question controller)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,4 +4,10 @@ class QuestionsController < ApplicationController
                      .map { |q| "<a href = #{test_question_path id: q.id} > #{q.text} </a>" }
                      .join('<br>').html_safe
   end
+
+  def show
+    question = Question.find(params[:id].to_i)
+    render html: "Question: #{question.text}<br>id: #{question.id} <br>"\
+      "Answers:<br>#{question.answers.map(&:text).join('<br>')}".html_safe
+  end
 end
