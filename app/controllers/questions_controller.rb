961d72c8581ed746ea4f01f7572e461ff38aba96
lenkton <antonov_s@yahoo.com>
2020-12-11 19:22:32 +0300

    added method to the Question controller to inspect the questions of the tests

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,7 @@
 class QuestionsController < ApplicationController
+  def index
+    render html: Test.find(params[:test_id].to_i).questions
+                     .map { |q| "<a href = #{test_question_path id: q.id} > #{q.text} </a>" }
+                     .join('<br>').html_safe
+  end
 end
