279f674cc8cef89c387438b119dcf9b4d4a492c6
lenkton <antonov_s@yahoo.com>
2020-12-23 16:44:06 +0300

    reworked the github_url helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,6 @@ module ApplicationHelper
   end
 
   def github_url(author, repo)
-    "https://github.com/#{author}/#{repo}/"
+    link_to repo, "https://github.com/#{author}/#{repo}/", rel: :nofollow, target: :_blank
   end
 end
