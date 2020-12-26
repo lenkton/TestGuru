dd83557a4dac67fe9b61f4634c00889f08da2bbc
lenkton <antonov_s@yahoo.com>
2020-12-26 21:44:00 +0300

    added a root path to the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'tests#index'
+
   resources :tests do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
