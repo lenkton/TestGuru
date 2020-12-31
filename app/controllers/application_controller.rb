760ad8ed4bbe76efb32ea0c61385cdd323235fa9
lenkton <antonov_s@yahoo.com>
2020-12-31 18:06:46 +0500

    added a requirement to be logged in to get access to the app resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  helper_method :current_user
+
+  private
+
+  def authenticate_user!
+    unless logged_in?
+      redirect_to login_path
+    end
+  end
+
+  def current_user
+    @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+  
+  def logged_in?
+    current_user.present?
+  end
 end
