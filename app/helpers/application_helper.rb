ee3a3d5132da36baa5e6b9a6193aa961fc704d48
lenkton <antonov_s@yahoo.com>
2021-01-04 03:53:35 +0500

    added flash alert helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,10 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to repo, "https://github.com/#{author}/#{repo}/", rel: :nofollow, target: :_blank
   end
+
+  def flash_alert
+    if flash[:alert]
+      content_tag :p, flash[:alert], class: 'flash alert'
+    end
+  end
 end
