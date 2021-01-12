60830f5f3a45cb1fe36e06deeb6e38833dcdaa03
lenkton <antonov_s@yahoo.com>
2021-01-12 19:54:56 +0500

    admin-implementation-connected reorganizations

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, path: :gurus, path_names: {sign_in: :login, sign_out: :logout}, controllers: {registrations: 'users/registrations'}
 
   resources :tests, only: :index do
-    resources :questions, shallow: true, except: :index do
-      resources :answers, shallow: true, except: :index
-    end
-
     member do
       post :start
     end
@@ -20,7 +16,11 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
-    resources :tests
+    resources :tests do
+      resources :questions, shallow: true, except: :index do
+        resources :answers, shallow: true, except: :index
+      end
+    end
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
