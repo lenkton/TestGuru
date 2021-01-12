85d120a82cfb1b7838ccf22fd569a6d0a531faeb
lenkton <antonov_s@yahoo.com>
2021-01-13 04:29:46 +0500

    implemented custom sign-in notice

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'tests#index'
   
-  devise_for :users, path: :gurus, path_names: {sign_in: :login, sign_out: :logout}, controllers: {registrations: 'users/registrations'}
+  devise_for :users,
+             path: :gurus,
+             path_names: {sign_in: :login, sign_out: :logout},
+             controllers: {registrations: 'users/registrations', sessions: 'users/sessions'}
 
   resources :tests, only: :index do
     member do
