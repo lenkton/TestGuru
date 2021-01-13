de02772551efc85d9fac8f914a7506aa9ed172ba
lenkton <antonov_s@yahoo.com>
2021-01-13 15:59:24 +0500

    added test creation mechanics

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -3,7 +3,28 @@ class Admin::TestsController < Admin::BaseController
     @tests = Test.all
   end
 
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    @test.author = current_user
+
+    if @test.save
+      redirect_to admin_test_path(@test)
+    else
+      render :new
+    end
+  end
+
   def show
     @test = Test.find(params[:id])
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:name, :category_id, :level)
   end
 end
