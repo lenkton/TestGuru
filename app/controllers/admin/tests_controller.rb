8dc5bfbeabbf284a130a84b63041dba773416e0e
lenkton <antonov_s@yahoo.com>
2021-01-14 00:46:30 +0500

    style fix

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -10,8 +10,8 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def create
-    @test = Test.new(test_params)
-    @test.author = current_user
+    
+    @test = current_user.tests.new(test_params)
 
     if @test.save
       redirect_to admin_test_path(@test)
