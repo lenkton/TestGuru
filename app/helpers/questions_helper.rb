478bbdadb02bb9c34bfb14d853c72755e7d5b245
lenkton <antonov_s@yahoo.com>
2021-01-18 16:31:04 +0300

    replaced custom function with the library truncate

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,17 +1,7 @@
 module QuestionsHelper
   def question_link(question)
-    link_name = question_link_name(question)
+    link_name = truncate(question.text, length: 25)
 
     link_to link_name, admin_question_path(question)
-  end
-
-  private
-
-  def question_link_name(question)
-    if question.text.length > 25
-      question.text[0, 25]
-    else
-      question.text
-    end
   end
 end
