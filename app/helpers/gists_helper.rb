422335c2c7d8fa29dd3d54388385b11a67b1be07
lenkton <antonov_s@yahoo.com>
2021-01-18 16:27:29 +0300

    relocated question_link helper from gist_helper to question_helper

--- app/helpers/gists_helper.rb
+++ app/helpers/gists_helper.rb
@@ -1,24 +1,8 @@
 module GistsHelper
-  def question_link(question)
-    link_name = question_link_name(question)
-
-    link_to link_name, admin_question_path(question)
-  end
-
   def gist_link(gist)
     link_to gist.gist_hash,
             gist.url,
             rel: :nofollow,
             target: :_blank
-  end
-
-  private
-
-  def question_link_name(question)
-    if question.text.length > 25
-      question.text[0, 25]
-    else
-      question.text
-    end
   end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,17 @@
 module QuestionsHelper
+  def question_link(question)
+    link_name = question_link_name(question)
+
+    link_to link_name, admin_question_path(question)
+  end
+
+  private
+
+  def question_link_name(question)
+    if question.text.length > 25
+      question.text[0, 25]
+    else
+      question.text
+    end
+  end
 end
