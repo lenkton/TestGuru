f779494bfcf3f5f6d8252c8a8ce5f7f0aa6bed84
lenkton <antonov_s@yahoo.com>
2021-01-20 16:34:03 +0300

    some optimizations in the checkConfirmation script

--- app/javascript/utilities/password_confirmation_check.js
+++ app/javascript/utilities/password_confirmation_check.js
@@ -1,26 +1,24 @@
 document.addEventListener('turbolinks:load', function(){
   var control = document.querySelector('.password-confirmation-check')
   if (control) control.addEventListener('input', checkConfirmation)
-
-  var control1 = document.querySelector('.password-confirmation-password')
-  if (control1) control1.addEventListener('input', checkConfirmation)
 })
 
 function checkConfirmation(){
-  original_field = document.querySelector('.password-confirmation-password')
-  confirmation_field = document.querySelector('.password-confirmation-check')
+  const originalField = this.parentElement.parentElement.querySelector('.password-confirmation-password')
+  errorMark = this.parentElement.querySelector('.error-mark')
+  successMark = this.parentElement.querySelector('.success-mark')
 
-  if(confirmation_field.value == ""){
-    document.querySelector('.error-mark').classList.add('hide')
-    document.querySelector('.success-mark').classList.add('hide')
+  if(this.value == ""){
+    errorMark.classList.add('hide')
+    successMark.classList.add('hide')
     return
   }
 
-  if(original_field.value == confirmation_field.value){
-    document.querySelector('.error-mark').classList.add('hide')
-    document.querySelector('.success-mark').classList.remove('hide')
+  if(originalField.value == this.value){
+    errorMark.classList.add('hide')
+    successMark.classList.remove('hide')
   } else {
-    document.querySelector('.error-mark').classList.remove('hide')
-    document.querySelector('.success-mark').classList.add('hide')
+    errorMark.classList.remove('hide')
+    successMark.classList.add('hide')
   }
 }
