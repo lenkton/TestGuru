320ec943777c2f2a630834a505a4db1dcc3b032f
lenkton <antonov_s@yahoo.com>
2021-01-21 15:42:30 +0300

    added basic js for editing tests names

---
+++ app/javascript/utilities/form_inline.js
@@ -0,0 +1,32 @@
+document.addEventListener('turbolinks:load', function(){
+  var controls = document.querySelectorAll('.form-inline-link')
+
+  if (controls.length){
+    for (var i = 0; i < controls.length; i++){
+      controls[i].addEventListener('click', formInlineLinkHandler)
+    }
+  }
+})
+
+function formInlineLinkHandler(event){
+  event.preventDefault()
+
+  const testId = this.dataset.testId
+  formInlineHandler(testId)
+}
+
+function formInlineHandler(testId){
+  var link = document.querySelector('.form-inline-link[data-test-id="' + testId +'"]')
+  var testName = document.querySelector('.test-name[data-test-id="' + testId +'"]')
+  var formInline = document.querySelector('.form-inline[data-test-id="' + testId +'"]')
+
+  if (formInline.classList.contains('hide')){
+    testName.classList.add('hide')
+    formInline.classList.remove('hide')
+    link.textContent = 'Cancel'
+  } else {
+    testName.classList.remove('hide')
+    formInline.classList.add('hide')
+    link.textContent = 'Edit'
+  }
+}
