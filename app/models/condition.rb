e62ffbd357d8b9dfad715bbc5e5c17c8eddb6b0c
lenkton <antonov_s@yahoo.com>
2021-01-30 17:21:00 +0300

    added a very raw conditions management

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -4,3 +4,7 @@ class Condition < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validates :type, presence: true
 end
+
+# need this to use Condition.subclasses
+require_dependency 'set_of_tests_solved_condition'
+require_dependency 'tries_count_condition'
