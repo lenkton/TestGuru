fa26c17bacbbf0c7f98e34a4118041b3b5ee4d60
lenkton <antonov_s@yahoo.com>
2021-02-05 14:46:59 +0300

    style fix

--- app/services/badges_granting_service.rb
+++ app/services/badges_granting_service.rb
@@ -1,6 +1,5 @@
 class BadgesGrantingService
-  CONDITIONS =
-  {
+  CONDITIONS = {
     tries_count: TriesCountConditionSpecification,
     category: CategoryConditionSpecification,
     level: LevelConditionSpecification
