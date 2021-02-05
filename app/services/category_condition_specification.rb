eb24ee68a7878d5fc844c15b51185753ad66135e
lenkton <antonov_s@yahoo.com>
2021-02-05 15:16:23 +0300

    somehow reworked category condition specification

--- app/services/category_condition_specification.rb
+++ app/services/category_condition_specification.rb
@@ -2,6 +2,12 @@ class CategoryConditionSpecification < AbstractConditionSpecification
   def satisfies?
     return if Category.find_by(id: @parameter) != @session.test.category
 
-    Category.find_by(id: @parameter).tests - @session.user.test_taking_sessions.successful.map(&:test) == []
+    Test.where(category_id: 1)
+        .joins(:test_taking_sessions)
+        .all? do |test|
+          test.test_taking_sessions
+              .where(user_id: 1, success: true)
+              .exists?
+        end
   end
 end
