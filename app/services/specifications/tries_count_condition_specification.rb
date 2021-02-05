6cb418a75cfde84de0c7b59c94e63ce9089419dc
lenkton <antonov_s@yahoo.com>
2021-02-05 15:44:32 +0300

    somehow reworked tries_count_condition_specification

--- app/services/specifications/tries_count_condition_specification.rb
+++ app/services/specifications/tries_count_condition_specification.rb
@@ -1,10 +1,11 @@
 class TriesCountConditionSpecification < AbstractConditionSpecification
   def satisfies?
-    tries_for_last_test = @session.user.test_taking_sessions.where('test_taking_sessions.test_id = ?', @session.test.id)
+    tries_for_last_test = @session.user
+                                  .test_taking_sessions
+                                  .where('test_taking_sessions.test_id = ?', @session.test.id)
 
-    return unless @session.successful?
-    return if tries_for_last_test.filter(&:successful?).size > 1
-
-    tries_for_last_test.size == @parameter.to_i
+    @session.successful? && tries_for_last_test.slice(0..-2).all? do |try|
+      !try.successful?
+    end && tries_for_last_test.size.equal?(@parameter.to_i)
   end
 end
