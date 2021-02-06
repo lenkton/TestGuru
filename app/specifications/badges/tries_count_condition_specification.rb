e92220f5449d1bebb2adfd9ab00ea45cb7ceac4a
lenkton <antonov_s@yahoo.com>
2021-02-07 00:08:48 +0300

    refactored tries count condition specification

--- app/specifications/badges/tries_count_condition_specification.rb
+++ app/specifications/badges/tries_count_condition_specification.rb
@@ -1,13 +1,34 @@
 module Badges
   class Badges::TriesCountConditionSpecification < Badges::AbstractConditionSpecification
     def satisfies?
-      tries_for_last_test = @test_taking_session.user
-                                    .test_taking_sessions
-                                    .where('test_taking_sessions.test_id = ?', @test_taking_session.test.id)
+      succeeded_now? && !succeeded_before? && had_exact_number_of_tries?
+    end
 
-      @test_taking_session.successful? && tries_for_last_test.slice(0..-2).all? do |try|
-        !try.successful?
-      end && tries_for_last_test.size.equal?(@parameter.to_i)
+    def succeeded_now?
+      @test_taking_session.successful?
+    end
+
+    def succeeded_before?
+      tries_for_last_test
+        .slice(0..-2)
+        .any?(&:successful?)
+    end
+
+    def had_exact_number_of_tries?
+      tries_for_last_test
+        .size
+        .equal?(@parameter.to_i)
+    end
+
+    def tries_for_last_test
+      @tries_for_last_test =
+        @test_taking_session
+          .user
+          .test_taking_sessions
+          .where(
+            'test_taking_sessions.test_id = ?',
+            @test_taking_session.test.id
+          )
     end
   end
 end
